dd1c6aada991c6acdb6b170243bc70c2b647c6b8
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2023-08-24 00:00:38 +0800

    build(#1): initial commit app setup

---
+++ src/main/java/app/phlane/phlane/PhlaneApplication.java
@@ -0,0 +1,13 @@
+package app.phlane.phlane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhlaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhlaneApplication.class, args);
+    }
+
+}
